b826a5c997d16f216442600dfd40e0ab96c284cf
Trevor Smith <104841977+tjs-cap@users.noreply.github.com>
2023-04-27 11:22:46 +0100

    Initial commit

---
+++ src/main/java/com/example/passingbetweenscenesjfx/Controller2.java
@@ -0,0 +1,2 @@
+package com.example.passingbetweenscenesjfx;public class Controller2 {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.passingbetweenscenesjfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.passingbetweenscenesjfx to javafx.fxml;
+    exports com.example.passingbetweenscenesjfx;
+}
