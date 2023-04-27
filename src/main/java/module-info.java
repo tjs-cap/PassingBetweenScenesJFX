dfb6abb5cc03e4a067ea51be8a69efb5ba8288df
Trevor Smith <104841977+tjs-cap@users.noreply.github.com>
2023-04-27 11:33:26 +0100

    Complete: controller1, controller2, layout1, layout2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.passingbetweenscenesjfx {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.passingbetweenscenesjfx to javafx.fxml;
     exports com.example.passingbetweenscenesjfx;
 }
